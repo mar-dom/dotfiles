13b9f71c9b6a63704bd25057af844d8afec85da7
Mario Domazet <mardom@mailbox.org>
2017-10-05 16:57:05 +0200

    checking for right exit code

--- install.sh
+++ install.sh
@@ -15,9 +15,16 @@ fi
 echo "[*] Pulling the git repo"
 # pull the dotfiles
 git clone https://github.com/mdomazet/dotfiles .dotfiles --recursive
+if [ ! $? -eq 0 ]; then
+    echo "Error: could not pull the repo!"
+    exit -1
+fi
 
 echo "[*] Linking files..."
 # install the dotfiles
 ~/.dotfiles/configure.sh
+
+echo "[*] Changing shell to zsh..."
+chsh -s $(which zsh)
 
 echo "[*] Done!"
