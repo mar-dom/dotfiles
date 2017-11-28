54d68c54c771c0a835598be593398d4f18824a54
Mario Domazet <mardom@mailbox.org>
2017-11-28 13:44:05 +0100

    added wallpaper, fonts, i3, i3status & ranger

--- configure.sh
+++ configure.sh
@@ -26,9 +26,9 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/dir_colors ~/.dir_colors
 
 # htop config
-mkdir -p ~/.config/htop
-if [ -f ~/.config/htop/htoprc ]; then
+if [ -d ~/.config/htop ]; then
         mv ~/.config/htop/htoprc ~/.config/localdotbackup/htoprc
+        mkdir -p ~/.config/htop
 fi
 ln -s ~/.dotfiles/config/htop/htoprc ~/.config/htop/htoprc
 
@@ -58,3 +58,38 @@ if [ -f ~/.zshrc ]; then
         mv ~/.zshrc ~/.config/localdotbackup/.zshrc
 fi
 ln -s ~/.dotfiles/zshrc ~/.zshrc
+
+# local files
+if [ -d ~/.local/wallpaper ]; then
+        mkdir -p /.config/localdotbackup/local
+        mv ~/.local/wallpaper ~/.config/localdotbackup/local/wallpaper
+fi
+mkdir -p ~/.local/wallpaper/
+ln -s ~/.dotfiles/local/wallpaper/wallpaper.jpg ~/.local/wallpaper/wallpaper.jpg
+
+if [ -d ~/.local/share/fonts ]; then
+        mkdir -p /.config/localdotbackup/local/share/fonts
+        mv ~/.local/share/fonts ~/.config/localdotbackup/local/share/fonts
+fi
+mkdir -p ~/.local/share
+ln -s ~/.dotfiles/local/share/fonts ~/.local/share/fonts
+
+# i3
+if [ -d ~/.config/i3 ]; then
+        mv ~/.config/i3 ~/.config/localdotbackup/i3
+fi
+ln -s ~/.dotfiles/config/i3 ~/.config/i3
+
+# i3status
+if [ -d ~/.config/i3status ]; then
+        mv ~/.config/i3status ~/.config/localdotbackup/i3status
+fi
+ln -s ~/.dotfiles/config/i3status ~/.config/i3status
+
+# ranger
+if [ -d ~/.config/ranger ]; then
+        mv ~/.config/ranger ~/.config/localdotbackup/ranger
+fi
+ln -s ~/.dotfiles/config/ranger ~/.config/ranger
+
+
