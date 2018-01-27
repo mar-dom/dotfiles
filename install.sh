e253aaf98fe91c2902a4b60e90d9e46396b17026
Mario Domazet <mardom@mailbox.org>
2018-01-27 15:48:02 +0100

    added ssh-config submodule

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ git clone https://github.com/zsh-users/antigen.git ~/.antigen
 echo "Vundle install..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "ssh install"
+if [[ -f ~/.ssh ]]; then mv ~/.ssh ~/.ssh.orig; fi
+cp -rf ~/.dotfiles/ssh-config ~/.ssh
+chmod 0600 .ssh/*.id
+
