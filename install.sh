106f22f8c96a46b40cdbf516658c513cbb728c5e
Mario Domazet <mardom@mailbox.org>
2018-01-27 15:40:18 +0100

    rearranged files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# set filetype=sh
+
+echo "Antigen install..."
+git clone https://github.com/zsh-users/antigen.git ~/.antigen
+
+echo "Vundle install..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
