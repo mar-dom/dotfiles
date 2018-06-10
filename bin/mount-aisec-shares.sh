9f730d5a6a91c96753a30636c29fc1b81c4aa087
Mario Domazet <mardom@mailbox.org>
2018-06-10 13:16:09 +0200

    updated for arch

--- bin/mount-aisec-shares.sh
+++ bin/mount-aisec-shares.sh
@@ -6,12 +6,12 @@ sudo mkdir -p /media/aisec/{AISEC,ISM,home}
 sudo chown -R domazet.domazet /media/aisec/
 
 USERNAME="domazet"
-PASSWORD=""
+PASSWORD=$(zenity --entry --title="Domain Password" --text="domazet@FILESERVER:" --hide-text)
 DOMAIN="AISEC"
 
-echo -n "Domain Password for domazet: " 
-read -s PASSWORD
-echo 
+#echo -n "Domain Password for domazet: " 
+#read -s PASSWORD
+#echo 
 
 sudo mount -t cifs -o vers=3.0,username=$USERNAME,domain=$DOMAIN,password=$PASSWORD,uid=1000,gid=1000 //fileserver.aisec.fraunhofer.de/AISEC /media/aisec/AISEC
 sudo mount -t cifs -o vers=3.0,username=$USERNAME,domain=$DOMAIN,password=$PASSWORD,uid=1000,gid=1000 //fileserver.aisec.fraunhofer.de/ISM /media/aisec/ISM
