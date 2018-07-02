1e2d95b9b0237124bbe07aa09d9d0b06284a557b
Mario Domazet <mardom@mailbox.org>
2018-07-02 10:42:12 +0200

    refresh dotfiles

--- bin/mount-aisec-shares.sh
+++ bin/mount-aisec-shares.sh
@@ -2,7 +2,7 @@
 
 /home/domazet/.dotfiles/bin/umount-aisec-shares.sh
 
-sudo mkdir -p /media/aisec/{AISEC,ISM,home}
+sudo mkdir -p /media/aisec/{AISEC,ISM,home,ISB}
 sudo chown -R domazet.domazet /media/aisec/
 
 USERNAME="domazet"
@@ -16,5 +16,5 @@ DOMAIN="AISEC"
 sudo mount -t cifs -o vers=3.0,username=$USERNAME,domain=$DOMAIN,password=$PASSWORD,uid=1000,gid=1000 //fileserver.aisec.fraunhofer.de/AISEC /media/aisec/AISEC
 sudo mount -t cifs -o vers=3.0,username=$USERNAME,domain=$DOMAIN,password=$PASSWORD,uid=1000,gid=1000 //fileserver.aisec.fraunhofer.de/ISM /media/aisec/ISM
 sudo mount -t cifs -o vers=3.0,username=$USERNAME,domain=$DOMAIN,password=$PASSWORD,uid=1000,gid=1000 //fileserver.aisec.fraunhofer.de/home/domazet /media/aisec/home
-
+sudo mount -t cifs -o vers=3.0,username=$USERNAME,domain=$DOMAIN,password=$PASSWORD,uid=1000,gid=1000 //fileserver.aisec.fraunhofer.de/ISB /media/aisec/ISB
 
